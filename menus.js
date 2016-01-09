0fb00d50046992d56db55644e5fcd8e32ba70655
Donald Averill <donaldaverill@gmail.com>
2016-01-09 13:37:48 -0800

    Fix spacing and remove console.log in menus.js

--- menus.js
+++ menus.js
@@ -1,14 +1,13 @@
 Menus = {};
 Menus.create = function() {
-	return new Mongo.Collection(null);
+  return new Mongo.Collection(null);
 };
 if (Package.templating) {
-	Package.templating.Template.registerHelper('Menus', (menu) => {
-    console.log(Menus[menu]);
+  Package.templating.Template.registerHelper('Menus', (menu) => {
     return Menus[menu].find({}, {
-			sort: {
-				weight: -1
-			}
-		});
-	});
+      sort: {
+        weight: -1
+      }
+    });
+  });
 }
